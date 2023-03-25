c6603b89f7d250f586c5b479c8f0affef2b856b6
Cristian Monroy <crismmr09@gmail.com>
2023-03-24 20:04:42 -0600

    Eliminados archivos que no deberían ir en main

--- Clase01/Tipado_Clase01.ts
+++
@@ -1,9 +0,0 @@
-let username: string
-
-username = "Cristian"
-username = "Cristian Monroy"
-
-let age: number;
-let isLogged: boolean;
-let listNames: string[] // Array<string>
-let listStudents: {name: string; age:number}[]
