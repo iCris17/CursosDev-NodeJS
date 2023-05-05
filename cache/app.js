5cc027cc4f8357c699e4fabdeec7b16bee5cf566
iCris17 <crismmr09@gmail.com>
2023-05-05 00:32:24 -0600

    Avances en la clase 7

--- cache/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.App = void 0;
-class App {
-    static generateApp() {
-        console.log("App generated");
-    }
-}
-exports.App = App;
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,9 @@ const server = new ServerBootstrap(app);
 (async ()=>{
     try {
         const listPromises = [server.initialize()]
-        const results = await Promise.all(listPromises);  
+        const results = await Promise.all(listPromises);
+        console.log("Everything is ready");
+        console.log("Server running")
     } catch (error) {
         console.log(error)
     }
